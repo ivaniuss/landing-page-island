a1ead931b675a15cd65787ffde0b9cd24ddb8576
ivaniuss <ivan.emi94@gmail.com>
2021-03-07 23:54:53 -0500

    corrections

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
 import PlaceToVisit from './components/PlaceToVisit';
+import bg from './assets/bg.jpg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+    backgroundImage: `url(${bg})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
--- src/static/places.js
+++ src/static/places.js
@@ -1,16 +1,18 @@
+import island1 from '../assets/island1.jpg'
+import island2 from '../assets/island2.jpg'
 const places = [
   {
     title: 'Green',
     description:
       "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
-    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+    imageUrl: `${island1}`,
     time: 1500,
   },
   {
     title: 'Bora Bora',
     description:
       'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
-    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    imageUrl: `${island2}`,
     time: 1500,
   },
 ];
